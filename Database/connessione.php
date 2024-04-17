b813900c98bce9b8c728577ce29cff7e8fc4fe9c
Angelo <boloangelosteven@itis-molinari.eu>
2024-04-17 12:45:37 +0200

    Aggiornamento

--- Database/connessione.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-
-  $connessione = new mysqli($host, $username, $password, $database);
-if ($connessione->connect_error) {
-    die("Connessione fallita: " . $connessione->connect_error);
-}
-?>
