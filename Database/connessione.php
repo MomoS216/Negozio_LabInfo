d376ddbd721d46e3be80ea6370c97dd301c6e043
Angelo <stevenbolo2005@gmail.com>
2024-04-17 11:41:40 +0200

    Push

--- Database/connessione.php
+++ Database/connessione.php
@@ -2,7 +2,6 @@
 
   $host = "mysql-2a82da0d-itis-a3fa.a.aivencloud.com";
   $username = "avnadmin";
-  $password = "AVNS_PnR6KUNHbwqpECrBaQR";
   $database = "NegozioDB";
 
   $connessione = new mysqli($host, $username, $password, $database);
