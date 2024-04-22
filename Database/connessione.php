1ff280f58f5a3ce4a68e3799d129da6ae47134a7
Angelo <boloangelosteven@itis-molinari.eu>
2024-04-22 16:01:58 +0200

    FunzioniAgg

--- Database/connessione.php
+++ Database/connessione.php
@@ -1,14 +1,34 @@
 <?php
-$config_file = file_get_contents('config.json', true);
-$config = json_decode( $config_file , true);
-$database_config = $config['database'];
-$host = $database_config['host'];
-$username = $database_config['username'];
-$password = $database_config['password'];
-$database = $database_config['database'];
 
-$connessione = new mysqli($host, $username, $password, $database);
-if ($connessione->connect_error) {
-  die("Connessione fallita: " . $connessione->connect_error);
+
+$host = 'mysql-2a82da0d-itis-a3fa.a.aivencloud.com';
+$user = 'avnadmin';
+$password = 'AVNS_PnR6KUNHbwqpECrBaQR';
+$database = 'NegozioDB';
+$port = 15544;
+
+
+$ssl_options = [
+    PDO::MYSQL_ATTR_SSL_CA => 'ca.pem',
+    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => true
+];
+
+try {
+
+    $connessione  = new PDO(
+        "mysql:host=$host;port=$port;dbname=$database",
+        $user,
+        $password,
+        [PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => true] + $ssl_options
+    );
+    
+
+    $connessione ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+
+    
+    echo "Connected successfully!";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
 ?>
